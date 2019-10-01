a7fcb15b674b0c20e36645835c498c4577b13975
Alexibarra999 <56003088+Alexibarra999@users.noreply.github.com>
2019-09-30 22:52:58 -0500

    Add files via upload

---
+++ multiplo.cs
@@ -0,0 +1,20 @@
+﻿//autor: José Alejandro Ibarra Miranda
+//fecha de creacion: 25/septiembre/19
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡¡¡HOLA MUNDO, AHORA EN C#!!!");
+            Console.ReadKey();
+        }
+    }
+}
