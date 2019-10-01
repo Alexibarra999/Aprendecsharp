65ae6fe69cc6108cb16835746e391c7e765043e3
Alexibarra999 <56003088+Alexibarra999@users.noreply.github.com>
2019-09-30 22:51:24 -0500

    Add files via upload

---
+++ entrada.cs
@@ -0,0 +1,34 @@
+﻿//autor: José Alejandro Ibarra Miranda
+//Fecha de creación: 25/septiembre/19
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string VALOR;
+            int R = 0;
+            Console.Write("Escribe: ");
+            VALOR = Console.ReadLine();
+
+            if (int.TryParse(VALOR, out R))
+            {
+                Console.WriteLine(string.Format("DATO ENTERO {0}, MUY BIEN!!", R));
+            }
+            else
+            {
+                Console.WriteLine("DATO NO ES ENTERO. INTENTAR DE NUEVO");
+            }
+           
+            Console.WriteLine("PRESIONE CUALQUIER TECLA PARA CONTINUAR...");
+            Console.ReadKey();
+           
+        }
+    }
+}
