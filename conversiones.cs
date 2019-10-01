9d635cefd4f2c08454431cefdaef1e60fb355ea4
Alexibarra999 <56003088+Alexibarra999@users.noreply.github.com>
2019-09-30 22:51:01 -0500

    Add files via upload

---
+++ conversiones.cs
@@ -0,0 +1,23 @@
+﻿//autor: José Alejandro Ibarra Miranda
+//Fecha de creación: 25/septiembre/19
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conversiones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = "1234";
+            Console.WriteLine(num.GetType().ToString());
+            int intnum = Convert.ToInt32(num);
+            Console.WriteLine(intnum.GetType().ToString());
+            Console.WriteLine(string.Format("el numero es {0}", intnum));
+            Console.ReadKey();
+        }
+    }
+}
