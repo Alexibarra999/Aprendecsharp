5f57cc7a1fd3ca9f4413e99fb1b48f3b2a4bcb53
Alexibarra999 <56003088+Alexibarra999@users.noreply.github.com>
2019-09-30 22:52:18 -0500

    Add files via upload

---
+++ tabla.cs
@@ -0,0 +1,29 @@
+﻿//autor: José Alejandro Ibarra Miranda
+//Fecha de creación: 25/septiembre/19
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string NUM;
+            int NUMERO;
+            Console.Write("ingresa un numero del 1 al 9: ");
+            NUM = Console.ReadLine();
+            NUMERO = Convert.ToInt32(NUM);
+            for (int i = 1; i <=10;  i++)
+            {
+                Console.WriteLine(string.Format("{0} x {1} = {2}", NUMERO, i, NUMERO * i)); 
+            }
+            Console.WriteLine("presiona cualquier tecla para terminar...");
+            Console.ReadKey();
+        }   
+             
+    }
+}
